baef885387fa4c57d5c1c79c437a679d80e84ae9
Pavan Suthar <53314417+pavansuthar567@users.noreply.github.com>
2024-10-29 00:57:45 +0530

    Testimonials Module changes
    Testimonials Module changes

--- src/models/testimonial.ts
+++ src/models/testimonial.ts
@@ -1,12 +1,15 @@
 import mongoose, { Schema, Document, Model } from 'mongoose';
+import { IUser } from './user';
 
 export interface ITestimonial extends Document {
   name: string;
   designation?: string; // Optional: Role or Position of the person giving the testimonial
   message: string;
   image?: string; // Optional: Image URL of the person
-  rating: number; // Rating out of 5
   createdAt: Date;
+  updatedAt: Date;
+  createdBy: IUser['_id'];
+  updatedBy: IUser['_id'];
 }
 
 const testimonialSchema = new Schema<ITestimonial>({
@@ -14,8 +17,14 @@ const testimonialSchema = new Schema<ITestimonial>({
   designation: { type: String },
   message: { type: String, required: true },
   image: { type: String },
-  rating: { type: Number, required: true, min: 1, max: 5 },
   createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  createdBy: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+  updatedBy: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+});
+
+testimonialSchema.pre('find', function () {
+  this.populate('createdBy updatedBy');
 });
 
 const TestimonialModel: Model<ITestimonial> = mongoose.model<ITestimonial>('testimonial', testimonialSchema);
