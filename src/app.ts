431f1c7f4970b4e2a366013fc966dc2d624d6b65
Pavan Suthar <53314417+pavansuthar567@users.noreply.github.com>
2025-02-02 11:08:10 +0530

    Webhook change
    Webhook change

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,27 @@ import cors from 'cors';
 import passport from 'passport';
 import initRoutes from './routes';
 import bodyParser from 'body-parser';
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import passportConfig from './config/passport';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../docs/swagger.json';
+import DashboardController from './components/dashboard/dashboard.controller';
+const router = express.Router();
 
 const app: Application = express();
 
 app.use(cors());
+
+/**
+ * @route POST /api/webhooks/tradingview
+ * @description Receive TradingView alerts via webhook
+ * @returns JSON
+ * @access Public (but secure with a secret key)
+ */
+router.post('/webhooks/tradingview', express.text({ type: '*/*' }), (req: Request, res: Response) => {
+  DashboardController.processWebhook(req, res);
+});
+
 app.use(express.json());
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true, parameterLimit: 50000 }));
